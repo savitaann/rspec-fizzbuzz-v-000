e3b38b0c3b1d6887490e87cbbb1474a1a185b471
savita madray <savitamadray01@gmail.com>
2017-10-14 02:45:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
 def fizzbuzz(int)
+elsif (int % 3 == 0) && (int % 5 == 0)
+  "FizzBuzz"
+  
   if int % 3 == 0
     "Fizz"
 
   elsif int % 5 == 0
     "Buzz"
 
-  elsif (int % 3 == 0) && (int % 5 == 0)
-    "FizzBuzz"
+  
 
-  else
-    nil
 
 
   end
